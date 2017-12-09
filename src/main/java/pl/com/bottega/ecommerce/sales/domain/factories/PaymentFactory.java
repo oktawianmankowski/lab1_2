c1bea43a22797a7eb436fe0a92ec7f9617e581cc
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2017-12-09 14:23:45 +0100

    Dodana fabryka Payment

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/factories/PaymentFactory.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.ecommerce.sales.domain.factories;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.payment.Payment;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class PaymentFactory {
+	public Payment createPayment(ClientData clientData, Money amount) {
+		Id aggregateId = Id.generate();
+		return new Payment(aggregateId, clientData, amount);
+	}
+}
--- src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
@@ -17,6 +17,7 @@ package pl.com.bottega.ecommerce.sales.domain.payment;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.factories.PaymentFactory;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class Payment {
@@ -27,6 +28,7 @@ public class Payment {
 
 	private Id aggregateId;
 
+	private PaymentFactory paymentFactory;
 
 	public Payment(Id aggregateId, ClientData clientData, Money amount) {
 		this.aggregateId = aggregateId;
@@ -35,8 +37,6 @@ public class Payment {
 	}
 
 	public Payment rollBack() {
-		Id id = Id.generate();
-
-		return new Payment(id, clientData, amount.multiplyBy(-1));		
+		return paymentFactory.createPayment(clientData, amount.multiplyBy(-1));
 	}
 }
