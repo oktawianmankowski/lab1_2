fd2d9d706a540127c25c242a910c4c8a4ac47c6d
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2017-12-09 17:51:53 +0100

    fix strategy pattern impementation

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/StandardTaxPolicy.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/StandardTaxPolicy.java
@@ -11,8 +11,23 @@ public class StandardTaxPolicy implements TaxPolicy {
         BigDecimal ratio = null;
         String desc = null;
 
-        ratio = BigDecimal.valueOf(0.23);
-        desc = "23%";
+        switch (requestItem.getProductData().getType()) {
+            case DRUG:
+                ratio = BigDecimal.valueOf(0.05);
+                desc = "5% (D)";
+                break;
+            case FOOD:
+                ratio = BigDecimal.valueOf(0.07);
+                desc = "7% (F)";
+                break;
+            case STANDARD:
+                ratio = BigDecimal.valueOf(0.23);
+                desc = "23%";
+                break;
+
+            default:
+                throw new IllegalArgumentException(requestItem.getProductData().getType() + " not handled");
+        }
 
         Money taxValue = net.multiplyBy(ratio);
 
